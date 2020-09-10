c368394f8356b5498da064cfe82273aa575fc9a0
akira.toyama <toyama.a.ab@gmail.com>
2020-09-10 17:55:01 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 遠山　哲 on 2020/09/10.
+//  Copyright © 2020 tootootooyaman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
